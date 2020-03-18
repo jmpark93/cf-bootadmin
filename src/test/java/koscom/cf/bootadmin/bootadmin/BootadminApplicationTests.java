21dc7cbf7a0e5bd2235680bb1ce695a5331a36f0
Jongmin Park <jmpark93@koscom.co.kr>
2020-03-18 16:58:45 +0900

    Initial commit

---
+++ src/test/java/koscom/cf/bootadmin/bootadmin/BootadminApplicationTests.java
@@ -0,0 +1,13 @@
+package koscom.cf.bootadmin.bootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
