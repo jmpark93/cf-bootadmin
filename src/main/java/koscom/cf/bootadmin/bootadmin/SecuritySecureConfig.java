bbb020e1a329bd7c57760f2bbcd69f041223b11f
Jongmin Park <jmpark93@koscom.co.kr>
2020-03-19 15:01:00 +0900

    make working...

--- src/main/java/koscom/cf/bootadmin/bootadmin/SecuritySecureConfig.java
+++ src/main/java/koscom/cf/bootadmin/bootadmin/SecuritySecureConfig.java
@@ -3,14 +3,11 @@ package koscom.cf.bootadmin.bootadmin;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.UUID;
 
 @Configuration(proxyBeanMethods = false)
 public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
