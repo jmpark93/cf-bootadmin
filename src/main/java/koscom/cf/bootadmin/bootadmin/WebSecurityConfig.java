8d114ef73b09d0262681325fcf8748fcdcda711a
박종민 <jmpark93@koscom.co.kr>
2020-06-12 18:11:30 +0900

    update ...

--- src/main/java/koscom/cf/bootadmin/bootadmin/WebSecurityConfig.java
+++ src/main/java/koscom/cf/bootadmin/bootadmin/WebSecurityConfig.java
@@ -1,16 +1,27 @@
 package koscom.cf.bootadmin.bootadmin;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.UUID;
+
 @Configuration(proxyBeanMethods = false)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.user.name}")
+    private String adminUser;
+
+    @Value("${spring.security.user.password}")
+    private String adminPassword;
+
     private final AdminServerProperties adminServer;
 
     public WebSecurityConfig(AdminServerProperties adminServer) {
@@ -38,15 +49,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         new AntPathRequestMatcher(this.adminServer.path("/instances"), HttpMethod.POST.toString()),
                         new AntPathRequestMatcher(this.adminServer.path("/instances/*"), HttpMethod.DELETE.toString()),
                         new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
-                );
-//                .and()
-//                .rememberMe().key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600);
+                )
+                .and()
+                .rememberMe().key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600);
     }
-//
-//    // Required to provide UserDetailsService for "remember functionality"
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
-//    }
+
+    // Required to provide UserDetailsService for "remember functionality"
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(adminUser).password("{noop}" + adminPassword).roles("USER");
+    }
 
 }
